4bcc54fbc4fd6ab8a095fb2cc316714ef2d84174
Shubhang Vats <shubhangvats14@gmail.com>
2020-10-01 10:32:27 +0530

    Update Deletion.c

--- Deletion.c
+++ Deletion.c
@@ -101,42 +101,58 @@ int main()
 
     fourth->data = 4;
     fourth->next = head;
+    
+    int n;
+    printf("Enter 1 for traverse only, 2 for delete the first node, 3 for insert at last node deletion, 4 for deleting at index, 5 for deleting a node at the node having a data value\n");
+    scanf("%d", &n);
+    switch(n)
+    {
+        case 1:
+            {
+                printf("Elements  of circular linked list are: \n");
+                traverse(head);
+                break;
+            }
+        case 2:
+            {
+                ////////////////@@@@@@@@@@@@   deleting first node   @@@@@@@@@@@@@///////////////////
+                printf("Elements  of circular linked list after deleting the first node are: \n");
+                head = deletingFirstNode(head);
+                traverse(head);    
+            }
+        case 3:
+            {
+                //////////////@@@@@@@@@@@@@@@   deleting last node    @@@@@@@@@@@@@@@//////////////
+                printf("Elements  of circular linked list after deleting the last node are: \n");
+                head = deletingLastNode(head);
+                traverse(head);
+                break;
+                
+            }
+        case 4:
+            {
+                ////////////          deleting in between node at an index      ///////////////////
+                int index = 2;
+                printf("Elements  of circular linked list after deleting the node at index %d are: \n", index);
+                head = deletingAtIndex(head, index);
+                traverse(head);
+                break;
+            }
+        case 5:
+            {
+                ////////////          deleting in between node having data value      ///////////////////
+                int data = 2;
+                printf("Elements  of circular linked list after deleting the node having data value = %d are: \n", data);
+                head = deletingAtData(head, data);
+                traverse(head);
+                break;
+            }
+        default:
+            {
+                printf("!!!!!!!!!!!ERROR!!!!!!!!!Wrong Choice, Try again\n");
+                break;
+            }
 
-    printf("Elements  of circular linked list are: \n");
-    traverse(head);
-
-
-    /*
-    ////////////////@@@@@@@@@@@@   deleting first node   @@@@@@@@@@@@@///////////////////
-    printf("Elements  of circular linked list after deleting the first node are: \n");
-    head = deletingFirstNode(head);
-    traverse(head);
-    */
-
-    /*
-    //////////////@@@@@@@@@@@@@@@   deleting last node    @@@@@@@@@@@@@@@//////////////
-    printf("Elements  of circular linked list after deleting the last node are: \n");
-    head = deletingLastNode(head);
-    traverse(head);
-    */
-
-
-    /*
-    ////////////          deleting in between node at an index      ///////////////////
-    int index = 2;
-    printf("Elements  of circular linked list after deleting the node at index %d are: \n", index);
-    head = deletingAtIndex(head, index);
-    traverse(head);
-    */
-
-
-
-    ////////////          deleting in between node having data value      ///////////////////
-    int data = 2;
-    printf("Elements  of circular linked list after deleting the node having data value = %d are: \n", data);
-    head = deletingAtData(head, data);
-    traverse(head);
-
-
+    }
     return 0;
 }
